0009d1448d68f614c2ec8f8a365be9c81da4c9dc
db-xm <139962245+db-xm@users.noreply.github.com>
2023-07-19 12:32:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,12 @@
 
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
+  default = "eu-west-1"
 }
 
 variable "prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  default = "dbxm"
 }
 
 provider "aws" {

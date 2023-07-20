d55733042f47bbe50546751811f17ddbe4528154
db-xm <139962245+db-xm@users.noreply.github.com>
2023-07-20 12:19:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,6 +55,14 @@ EOF
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_object" "bucket" {
   acl          = "public-read"

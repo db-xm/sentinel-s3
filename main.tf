0378eda04289412ca0102a6d2a4cc65463710eab
db-xm <139962245+db-xm@users.noreply.github.com>
2023-10-04 16:32:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,6 +115,36 @@ resource "aws_dynamodb_table" "dynamodb" {
   server_side_encryption {
     enabled = false
   }
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
 }
 
 resource "aws_dax_cluster" "dax" {

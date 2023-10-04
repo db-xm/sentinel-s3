1b157c0a81191c3335ed9d3647eee8a44a9a11a1
db-xm <139962245+db-xm@users.noreply.github.com>
2023-10-04 16:14:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,11 @@ resource "aws_cloudfront_distribution" "cloudfront" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "iactest"
     cache_policy_id = aws_cloudfront_cache_policy.cache_policy.id
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 }
 
